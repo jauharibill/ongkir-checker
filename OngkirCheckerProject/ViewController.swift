71b326443492ad3f00c0acad209f7b179362c1ab
Bill Tanthowi Jauhari <bill.tanthowi.j@gmail.com>
2020-02-22 14:32:13 +0700

    Initial Commit

---
+++ OngkirCheckerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OngkirCheckerProject
+//
+//  Created by Bill Tanthowi Jauhari on 22/02/20.
+//  Copyright © 2020 TechnologueID. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
